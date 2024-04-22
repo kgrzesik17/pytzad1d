1b9ffebfbbe6205e13306b1bb074c2a28a05b4eb
Kacper <kgrzesik17@gmail.com>
2024-04-22 18:17:24 +0200

    'create' module now outputs what was created.

--- operacje_na_plikach.py
+++ operacje_na_plikach.py
@@ -7,14 +7,20 @@ def create(name, type, content=''):
     Arguments:
     name - name of a folder you want to create.
     type - file extension. If set to "folder", then the module is going to create a folder.
-    content - optional - only if type != folder. Lets you write content for the file.
+    content (optional) -  only if type != folder. Lets you write content for the file.
+
+    Output:
+    bool - 1 if a file was created, 0 if a folder was created.
+    
     '''
 
     if type == 'folder'.lower():
         os.mkdir(name)
+        return 0
 
     else:
         fullname = name + '.' + type
         file = open(fullname, 'x')
         file.write(content)
         file.close()
+        return 1
